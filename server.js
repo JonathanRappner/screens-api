4b7afe7a24adc7ede9952be16ab290ecdb225923
Jonathan Rappner <jrappner@gmail.com>
2021-05-13 21:28:31 +0200

    Routes refactoring

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express")
 const app = express()
+const cars = require('./routes/cars')
+const fruits = require('./routes/fruits')
+
+// Variables
 const port = 3000
-const fruits = ['banana', 'orange', 'kiwi']
+
 
 
 // Middleware that runs on every request
@@ -12,15 +16,10 @@ app.use((req, res, next) => {
 
 
 
-app.use('/car', (req, res, next) => {
-	console.log('Something about cars')
-	next()
-})
+app.use('/car', cars)
+app.use('/fruits?', fruits)
 
-app.use('/fruits?', (req, res, next) => {
-	console.log('Something about fruits')
-	next()
-})
+
 
 
 // Root
@@ -28,23 +27,7 @@ app.get('/', (req, res) => {
 	res.send('root')
 })
 
-// GET fruit or fruits
-app.get('/fruits?/:fruit_id(\\d*)', (req, res) => {
-	if(req.params.fruit_id <= (fruits.length - 1))
-		res.json(fruits[req.params.fruit_id])
-	else
-		res.status(500).json({'message': 'Index out of bounds, yo'})
-})
 
-// GET fruit or fruits
-app.get(/fruits?/, (req, res) => {
-	res.status(200).json({fruits})
-})
-
-// Car name and nbr of wheels
-app.get('/car/:car_name([A-Za-z]+)/:wheels(\\d+)', (req, res) => {
-	res.send(`Car: ${req.params.car_name}\nWheels: ${req.params.wheels}`)
-})
 
 
 // Start listening for requests...
