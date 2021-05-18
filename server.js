1757d92c9e96f2548687ff88333ec26abaff6e93
Jonathan Rappner <jrappner@gmail.com>
2021-05-18 21:15:54 +0200

    Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const port = 3000
 
 // Middleware that runs on every request
 app.use((req, res, next) => {
-	// res.header("Access-Control-Allow-Origin", "*");
-	// res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
 	// logging
 	const now = moment().format('YYYY-MM-DD HH:mm:ss')
