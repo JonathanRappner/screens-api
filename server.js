c18e2da04ab9f6c84ff58d069f126951e9664add
Jonathan Rappner <jrappner@gmail.com>
2021-06-23 17:35:19 +0200

    json decode, handle requests from localhost

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const chalk = require('chalk') // console.log() colors
 // Express
 const express = require('express') // API framework
 const app = express()
+app.use(express.urlencoded({extended: true})); // parse:a body-värden i requests
+app.use(express.json()); // hantera json-värden i requests
 
 
 // Routes
@@ -11,7 +13,8 @@ const screens = require('./routes/screens.route')
 const games = require('./routes/games.route')
 
 // Variables
-const port = 3000
+const port = 4000
+const methods = ['get', 'post', 'put', 'delete'] // skriv inte ut head och options-metoderna
 
 // Middleware that runs on every request
 app.use((req, res, next) => {
@@ -19,9 +22,18 @@ app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
 	// logging
-	const now = moment().format('YYYY-MM-DD HH:mm:ss')
-	const ipv4 = req.ip.match(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/)[0]
-	console.log(chalk.green(req.method), chalk.yellow(req.url), chalk.red(ipv4), chalk.blue(`(${now})`))
+	if(methods.includes(req.method.toLowerCase()))
+	{
+		const now = moment().format('YYYY-MM-DD HH:mm:ss')
+		const ipv4_matches = req.ip.match(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/)
+		const ipv4 = ipv4_matches !== null ? ipv4_matches[0] : 'localhost' // hämtar IPv4-delen ur req.ip, om ingen IPv4 finns, skriv ut 'localhost'
+		console.log(
+			chalk.green(req.method), // http-metod (GET, POST, osv.)
+			chalk.yellow(req.url), // url
+			chalk.red(ipv4), // IP
+			chalk.blue(`(${now})`) // tid
+		)
+	}
 	next() // run next middleware
 })
 
