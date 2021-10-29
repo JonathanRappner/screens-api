6a19c7309f4af2ada11a50eb93fb7af09a77ee85
Jonathan Rappner <jrappner@gmail.com>
2021-10-29 17:10:49 +0200

    mysql connection => pool

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const config = require('./config')
 const mysql = require('mysql2')
-const db = mysql.createConnection(config.db).promise()
+const db = mysql.createPool(config.db).promise()
 
 module.exports = db
