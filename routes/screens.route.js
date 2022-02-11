2bea4379d4db2c1a9396e67a1be1497acd68219b
Jonathan Rappner <jrappner@gmail.com>
2022-02-11 12:53:59 +0100

    merged routes for all and game_filter

--- routes/screens.route.js
+++ routes/screens.route.js
@@ -5,17 +5,24 @@ const screens_service = require('../services/screens.service')
 
 
 /** 
- * Get screenshots without game filter
- * Example: screens/all/1621107351/8 (eight screens incl. 1621107351 and older)
- * Example: screens/all/latest/4 (4 latest/newest screens)
+ * Get screenshots with game filter
+ * Example: screens/me/1621107351/8 (eight screens incl. 1621107351 and older)
+ * Example: screens/me2/latest/4 (4 latest/newest Mass Effect 2 screens)
  */
-router.get('/all/:start(\\d{10}|latest)/:length(\\d+)', async (req, res) => {
+router.get('/:game_filter(\\w+)/:start(\\d{10}|latest)/:length(\\d+)', async (req, res) => {
+	const game_filter = req.params.game_filter // game code
 	const start = req.params.start // this screen id and earlier
 	const length = parseInt(req.params.length)
 
-	const where_statement = start == 'latest'
+	// where length
+	let where_statement = start == 'latest'
 		? ''
-		: 'WHERE s.id <= ' + start
+		: `AND s.id <= ${start}`
+
+	// where game_filter
+	where_statement += game_filter === 'all'
+		? ''
+		: 'AND s.game_code = '+ db.escape(game_filter)
 
 	const [rows] = await db.query(
 		`SELECT
@@ -24,7 +31,9 @@ router.get('/all/:start(\\d{10}|latest)/:length(\\d+)', async (req, res) => {
 		FROM screens s
 		INNER JOIN screens_games g
 			ON s.game_code = g.code
-		${where_statement}
+		WHERE
+			1=1
+			${where_statement}
 		ORDER BY s.id DESC
 		LIMIT ?`,
 		length
@@ -41,45 +50,7 @@ router.get('/all/:start(\\d{10}|latest)/:length(\\d+)', async (req, res) => {
 })
 
 /** 
- * Get screenshots with game filter
- * Example: screens/me/1621107351/8 (eight screens incl. 1621107351 and older)
- * Example: screens/me2/latest/4 (4 latest/newest Mass Effect 2 screens)
- */
-router.get('/:game_filter(\\w+)/:start(\\d{10}|latest)/:length(\\d+)', async (req, res) => {
-	const game_filter = req.params.game_filter // game code
-	const start = req.params.start // this screen id and earlier
-	const length = parseInt(req.params.length)
-
-	const where_statement = start == 'latest'
-		? ''
-		: `s.id <= ${start} AND`
-
-	const [rows] = await db.query(
-		`SELECT
-			s.id, s.date_time, s.file_name, s.game_code, s.width, s.height, s.description,
-			g.name game_name, g.icon16
-		FROM screens s
-		INNER JOIN screens_games g
-			ON s.game_code = g.code
-		WHERE
-			${where_statement}
-			s.game_code = ?
-		ORDER BY s.id DESC
-		LIMIT ?`,
-		[game_filter, length]
-	)
-
-	// if(error) res.status(500).json({message: error})
-
-	if (rows.length) {
-		data = rows.map(row => screens_service.process(row)) // process each row
-		res.json(data) // success
-	}
-	else
-		res.status(500)
-})
-
-/** Get single screenshot's data
+ * Get single screenshot's data
  * Example screens/1623870259
  */
 router.get('/:id(\\d{10})', async (req, res) => {
